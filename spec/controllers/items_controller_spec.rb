d9718e769574ca3e30cbad68a34ce43420e36126
Rafal Piekara <rafal@piekara.me>
2022-11-13 00:00:02 +0100

    Fix specs with user context

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 describe ItemsController do
+  let(:user) { create(:user) }
+
+  before do
+    sign_in user
+  end
+
   describe 'GET #index' do
     subject(:index_request) { get :index }
 
@@ -18,7 +24,7 @@ describe ItemsController do
   describe 'GET #show' do
     subject(:show_request) { get :show, params: { id: item.id } }
 
-    let(:item) { create(:item) }
+    let(:item) { create(:item, user: user) }
 
     it 'returns http success' do
       show_request
@@ -48,7 +54,7 @@ describe ItemsController do
   describe 'GET #edit' do
     subject(:edit_request) { get :edit, params: { id: item.id } }
 
-    let(:item) { create(:item) }
+    let(:item) { create(:item, user: user) }
 
     it 'returns http success' do
       edit_request
@@ -84,7 +90,7 @@ describe ItemsController do
   describe 'PUT #update' do
     subject(:update_request) { put :update, params: { id: item.id, item: new_attributes } }
 
-    let(:item) { create(:item) }
+    let(:item) { create(:item, user: user) }
     let(:new_attributes) { attributes_for(:item) }
 
     it 'returns http redirect' do
@@ -105,7 +111,7 @@ describe ItemsController do
   describe 'DELETE #destroy' do
     subject(:destroy_request) { delete :destroy, params: { id: item.id } }
 
-    let!(:item) { create(:item) }
+    let!(:item) { create(:item, user: user) }
 
     it 'returns https redirect' do
       destroy_request
